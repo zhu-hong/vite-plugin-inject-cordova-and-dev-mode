c4286660eb563bc9f3dbbd56b234af0f38feb9a7
朱鸿 <zhxhyy@qq.com>
2025-09-28 13:04:13 +0800

    使用relative获取html位置

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { HtmlTagDescriptor, Plugin } from 'vite'
 import { readFile, writeFile } from 'node:fs/promises'
-import { resolve, parse, dirname } from 'node:path'
+import { resolve, parse, dirname, relative } from 'node:path'
 
 interface IPluginConfig {
   /**
@@ -23,8 +23,7 @@ export const injectCordovaAndDevModePlugin: (config?: IPluginConfig) => Plugin[]
   const headMatch = /([ \t]*)<head[^>]*>/i
 
   const injectScript = async (filepath: string) => {
-    const pwd = process.cwd()
-    filepath = filepath.replace(pwd + '/', '')
+    filepath = relative(process.cwd(), filepath)
 
     const outputHtml = resolve(outDir, filepath)
     let html = String(await readFile(outputHtml))
@@ -66,7 +65,7 @@ export const injectCordovaAndDevModePlugin: (config?: IPluginConfig) => Plugin[]
       enforce: 'post',
       configResolved: (cfg) => {
         outDir = cfg.build.outDir
-        inputs = cfg.build.rollupOptions.input ?? resolve(process.cwd(), 'index.html')
+        inputs = cfg.build.rollupOptions.input ?? resolve('index.html')
       },
       writeBundle: async () => {
         if (typeof inputs === 'string') {
